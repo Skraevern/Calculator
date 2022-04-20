b9eaae6a40c35c09f836cca96000dbf8f9cc29dd
Skraevern <kristian.skreosen@icloud.com>
2022-04-20 20:27:31 +0200

    Added variables for all buttons and text

---
+++ script.js
@@ -0,0 +1,23 @@
+`use strict`;
+
+const text = document.getElementById(`text`);
+const btn0 = document.getElementById(`btn-0`);
+const btn1 = document.getElementById(`btn-1`);
+const btn2 = document.getElementById(`btn-2`);
+const btn3 = document.getElementById(`btn-3`);
+const btn4 = document.getElementById(`btn-4`);
+const btn5 = document.getElementById(`btn-5`);
+const btn6 = document.getElementById(`btn-6`);
+const btn7 = document.getElementById(`btn-7`);
+const btn8 = document.getElementById(`btn-8`);
+const btn9 = document.getElementById(`btn-9`);
+const substractBtn = document.getElementById(`substract-btn`);
+const multiplyBtn = document.getElementById(`multiply-btn`);
+const percentBtn = document.getElementById(`percent-btn`);
+const divideBtn = document.getElementById(`divide-btn`);
+const equalsBtn = document.getElementById(`equals=btn`);
+const rootBtn = document.getElementById(`root-btn`);
+const dotBtn = document.getElementById(`dot-btn`);
+const addBtn = document.getElementById(`add-btn`);
+const acBtn = document.getElementById(`ac-btn`);
+const cBtn = document.getElementById(`c-btn`);
