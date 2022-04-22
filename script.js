0767a086f4ca166144e5bea7d10ba3661b734158
Skraevern <86933999+Skraevern@users.noreply.github.com>
2022-04-23 00:00:31 +0200

    Put most of functions inside calculator

--- script.js
+++ script.js
@@ -29,174 +29,163 @@ const mcBtn = document.getElementById(`mc-btn`);
 const plussMinusBtn = document.getElementById(`plus/minus-btn`);
 
 const calculator = {
-  displayValue: ``,
-  firstOperand: null,
-  waitingForSecondOperand: true,
-  operator: null,
-  numberContainsDot: false,
-  memory: null,
-  calculate() {
-    if (this.operator === `+`) {
-      this.firstOperand += Number(this.displayValue);
-    }
-    if (this.operator === `-`) {
-      this.firstOperand -= Number(this.displayValue);
-    }
-    if (this.operator === `*`) {
-      this.firstOperand *= Number(this.displayValue);
-    }
-    if (this.operator === `/`) {
-      this.firstOperand /= Number(this.displayValue);
-    }
-
-    this.displayValue = this.firstOperand;
+    displayValue: ``,
+    firstOperand: null,
+    waitingForSecondOperand: true,
+    operator: null,
+    numberContainsDot: false,
+    memory: null,
+    displayText() {
+      if (
+        this.displayValue.toString().length > 8 &&
+        this.firstOperand !== null
+      ) {
+        text.textContent = this.displayValue.toString().slice(0, 8) + `...`;
+      } else {
+        text.textContent = this.displayValue;
+      }
+    },
+    calculate() {
+      if (this.operator === `+`) {
+        this.firstOperand += Number(this.displayValue);
+      }
+      if (this.operator === `-`) {
+        this.firstOperand -= Number(this.displayValue);
+      }
+      if (this.operator === `*`) {
+        this.firstOperand *= Number(this.displayValue);
+      }
+      if (this.operator === `/`) {
+        this.firstOperand /= Number(this.displayValue);
+      }
+      this.displayValue = this.firstOperand;
+    },
+    calculateRoot() {
+      this.displayValue = Math.sqrt(this.firstOperand);
+    },
+    deleteAll() {
+      this.displayValue = ``;
+      this.firstOperand = null;
+      this.waitingForSecondOperand = true;
+      this.operator = null;
+      this.numberContainsDot = false;
+      this.displayText();
+    },
+    delete() {
+      calculator.displayValue = ``;
+      calculator.displayText();
+    },
+    calculateMemory(e) {
+      if ((e === `-` || e === `memory-recall`) && this.memory === null) {
+        return;
+      }
+      if (e === `+`) {
+        if (this.memory === null) {
+          this.memory = Number(this.displayValue);
+          memTxt.textContent = `MEMORY`;
+        } else {
+          this.memory += Number(this.displayValue);
+        }
+      }
+      if (e === `-`) {
+        this.memory -= Number(calculator.displayValue);
+      }
+      if (e === `memory-recall`) {
+        this.displayValue = this.memory;
+        this.displayText();
+      }
+      if (e === `memory-clear`) {
+        this.memory = null;
+        memTxt.textContent = ``;
+      }
+    },
+    plusMinus() {
+      if (this.displayValue === `` || this.displayValue === `0.`) {
+        return;
+      } else {
+        this.displayValue =
+          Number(this.displayValue) - Number(this.displayValue) * 2;
+        this.displayText();
+      }
+    },
+    percentage() {
+      if (calculator.displayValue === `` || calculator.displayValue === `0.`) {
+        return;
+      }
+      if (calculator.operator === null) {
+        calculator.displayValue = Number(calculator.displayValue) / 100;
+      }
+      if (calculator.operator === `*`) {
+        calculator.displayValue =
+          (Number(calculator.displayValue) * calculator.firstOperand) / 100;
+      }
+      if (calculator.operator === `+`) {
+        calculator.displayValue =
+          (Number(calculator.displayValue) * calculator.firstOperand) / 100 +
+          calculator.firstOperand;
+      }
+      if (calculator.operator === `-`) {
+        calculator.displayValue =
+          calculator.firstOperand -
+          (Number(calculator.displayValue) * calculator.firstOperand) / 100;
+      }
+      calculator.displayText();
+    },
   },
-  calculateRoot() {
-    this.displayValue = Math.sqrt(this.firstOperand);
-  },
-};
-
-displayText = () => {
-  if (
-    calculator.displayValue.toString().length > 8 &&
-    calculator.firstOperand !== null
-  ) {
-    text.textContent = calculator.displayValue.toString().slice(0, 8) + `...`;
-  } else {
-    text.textContent = calculator.displayValue;
-  }
-};
-
-btnPressed = (e) => {
-  if (calculator.displayValue === `` && e === `.`) {
-    calculator.displayValue = `0.`;
-    displayText();
-    return;
-  }
-
-  // Prevents numbers user inputs overflowing.
-  if (
-    calculator.displayValue.length > 8 ||
-    (calculator.displayValue === `` && e === `0`) ||
-    (e === `.` && calculator.numberContainsDot === true)
-  ) {
-    return;
-  }
-
-  // check if input is number. If it`s i number adds it to a string.
-  if (!isNaN(e) || e === `.` || e === `0.`) {
-    calculator.displayValue += e;
-    displayText();
-    if (e === `.`) {
-      calculator.numberContainsDot = true;
+  btnPressed = (e) => {
+    if (calculator.displayValue === `` && e === `.`) {
+      calculator.displayValue = `0.`;
+      calculator.displayText();
+      return;
     }
-  }
 
-  // Stores first number in calculater object
-  if (
-    (e === `+` || e === `-` || e === `*` || e === `/`) &&
-    calculator.waitingForSecondOperand
-  ) {
-    calculator.firstOperand = Number(calculator.displayValue);
-    calculator.waitingForSecondOperand = false;
-    calculator.displayValue = ``;
-    calculator.numberContainsDot = false;
-  }
-  // if calculator object has a stored number. Calculates.
-  if (
-    (e === `+` || e === `-` || e === `*` || e === `/` || e === `=`) &&
-    calculator.waitingForSecondOperand === false
-  ) {
-    calculator.calculate();
-    displayText();
-    calculator.operator = e;
-    calculator.displayValue = ``;
-  }
-  if (e === `root`) {
-    calculator.firstOperand = Number(calculator.displayValue);
-    calculator.calculateRoot();
-    displayText();
-  }
-};
+    // Prevents numbers user inputs overflowing.
+    if (
+      calculator.displayValue.length > 8 ||
+      (calculator.displayValue === `` && e === `0`) ||
+      (e === `.` && calculator.numberContainsDot === true)
+    ) {
+      return;
+    }
+
+    // check if input is number. If it`s i number adds it to a string.
+    if (!isNaN(e) || e === `.` || e === `0.`) {
+      calculator.displayValue += e;
+      calculator.displayText();
+      if (e === `.`) {
+        calculator.numberContainsDot = true;
+      }
+    }
+
+    // Stores first number in calculater object
+    if (
+      (e === `+` || e === `-` || e === `*` || e === `/`) &&
+      calculator.waitingForSecondOperand
+    ) {
+      calculator.firstOperand = Number(calculator.displayValue);
+      calculator.waitingForSecondOperand = false;
+      calculator.displayValue = ``;
+      calculator.numberContainsDot = false;
+    }
+    // if calculator object has a stored number. Calculates.
+    if (
+      (e === `+` || e === `-` || e === `*` || e === `/` || e === `=`) &&
+      calculator.waitingForSecondOperand === false
+    ) {
+      calculator.calculate();
+      calculator.displayText();
+      calculator.operator = e;
+      calculator.displayValue = ``;
+    }
+    if (e === `root`) {
+      calculator.firstOperand = Number(calculator.displayValue);
+      calculator.calculateRoot();
+      calculator.displayText();
+    }
+  };
 
 // Delete
-acBtn.onclick = () => {
-  calculator.displayValue = ``;
-  calculator.firstOperand = null;
-  calculator.waitingForSecondOperand = true;
-  calculator.operator = null;
-  calculator.numberContainsDot = false;
-  displayText();
-};
 
-cBtn.onclick = () => {
-  calculator.displayValue = ``;
-  displayText();
-};
-mPlussBtn.onclick = () => {
-  if (calculator.memory === null) {
-    calculator.memory = Number(calculator.displayValue);
-    memTxt.textContent = `MEMORY`;
-  } else {
-    calculator.memory += Number(calculator.displayValue);
-  }
-};
-
-memBtn.onclick = () => {
-  if (calculator.memory === null) {
-    return;
-  } else {
-    calculator.displayValue = calculator.memory;
-    displayText();
-  }
-};
-mMinusBtn.onclick = () => {
-  if (calculator.memory === null) {
-    return;
-  } else {
-    calculator.memory -= Number(calculator.displayValue);
-  }
-};
-
-mcBtn.onclick = () => {
-  calculator.memory = null;
-  memTxt.textContent = ``;
-};
-
-plussMinusBtn.onclick = () => {
-  if (calculator.displayValue === `` || calculator.displayValue === `0.`) {
-    return;
-  }
-  calculator.displayValue =
-    Number(calculator.displayValue) - Number(calculator.displayValue) * 2;
-  displayText();
-};
-
-percentBtn.onclick = () => {
-  console.log(calculator);
-  if (calculator.displayValue === `` || calculator.displayValue === `0.`) {
-    return;
-  }
-  if (calculator.operator === null) {
-    calculator.displayValue = Number(calculator.displayValue) / 100;
-  }
-  if (calculator.operator === `*`) {
-    calculator.displayValue =
-      (Number(calculator.displayValue) * calculator.firstOperand) / 100;
-  }
-  if (calculator.operator === `+`) {
-    calculator.displayValue =
-      (Number(calculator.displayValue) * calculator.firstOperand) / 100 +
-      calculator.firstOperand;
-  }
-  if (calculator.operator === `-`) {
-    calculator.displayValue =
-      calculator.firstOperand -
-      (Number(calculator.displayValue) * calculator.firstOperand) / 100;
-  }
-  displayText();
-};
-rootBtn.onclick = () => btnPressed(`root`);
 btn1.onclick = () => btnPressed(`1`);
 btn2.onclick = () => btnPressed(`2`);
 btn3.onclick = () => btnPressed(`3`);
@@ -209,7 +198,16 @@ btn9.onclick = () => btnPressed(`9`);
 btn0.onclick = () => btnPressed(`0`);
 dotBtn.onclick = () => btnPressed(`.`);
 addBtn.onclick = () => btnPressed(`+`);
+cBtn.onclick = () => calculator.delete();
 equalsBtn.onclick = () => btnPressed(`=`);
 divideBtn.onclick = () => btnPressed(`/`);
+rootBtn.onclick = () => btnPressed(`root`);
 multiplyBtn.onclick = () => btnPressed(`*`);
 subtractBtn.onclick = () => btnPressed(`-`);
+acBtn.onclick = () => calculator.deleteAll();
+percentBtn.onclick = () => calculator.percentage();
+plussMinusBtn.onclick = () => calculator.plusMinus();
+mMinusBtn.onclick = () => calculator.calculateMemory(`-`);
+mPlussBtn.onclick = () => calculator.calculateMemory(`+`);
+mcBtn.onclick = () => calculator.calculateMemory(`memory-clear`);
+memBtn.onclick = () => calculator.calculateMemory(`memory-recall`);
